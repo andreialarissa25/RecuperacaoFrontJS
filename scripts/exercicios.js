12e2f1400b1251662e6428c5f789f1d921581d88
andreia <andreialarissac@gmail.com>
2026-03-26 20:58:38 -0300

    avaliacao prof renan

---
+++ scripts/exercicios.js
@@ -0,0 +1,41 @@
+//questao 1 Peça ao usuário o nome de um produto e o preço. Mostre uma mensagem dizendo quanto custa o produto.
+function exercicio1(){
+const Numero= Number(prompt("digite um numero: "))
+const outroNumero= ("digite outro numero: ")
+const resultado = (Numero + outroNumero)
+alert= resultado
+}
+const buttonExercicio1 = document.getElementById("exercicio1")
+buttonExercicio1.addEventListener('click', () => {exercicio1()})
+
+//questao 2. Peça o comprimento e a largura de um terreno. Calcule e mostre a quantidade de metros quadrados.
+function exercicio2(){
+const comprimento= ("digite o comprimento")
+const largura = ("digite a largura")
+
+
+const buttonExercicio2 = document.getElementById("exercicio2")
+buttonExercicio2.addEventListener('click', () => {exercicio2()})
+
+}
+
+
+
+
+
+//questao 13. Peça dois números e informe se o primeiro é divisível pelo segundo.
+
+
+
+
+
+//questao 11. Peça a temperatura atual. Informe se esta Frio 
+
+
+
+
+
+
+
+
+//questao2. Peça o comprimento e a largura de um terreno. Calcule e mostre a quantidade de metros quadrados.
